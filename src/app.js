765d520c86bc61c1000d1f470cfcf0e191c225ad
ararize <gkpapakotsis@gmail.com>
2026-06-11 16:07:29 +0300

    thats the merge we made

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import  express from 'express';
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from our acquisition server!');
+});
+
+export default app;
